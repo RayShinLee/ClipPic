b7c9d1dac6882352420d04253b53da5b71765528
RayShinLee <rayshinlee@hotmail.com>
2022-06-24 15:56:14 +0800

    [feature][LoginSignUp VC] Create Log in and Sign up View Controller

---
+++ ClipPic/ClipPic/ViewController/Account/LoginSignUpViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginSignUpViewController.swift
+//  ClipPic
+//
+//  Created by RayShin Lee on 2022/6/24.
+//
+
+import UIKit
+
+class LoginSignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
