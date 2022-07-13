7bafc6cbbee259e5ca09e599d898651db947aa63
RayShinLee <rayshinlee@hotmail.com>
2022-07-13 23:45:24 +0800

    [Extension] Add showError func in extension file

---
+++ ClipPic/ClipPic/ViewController/Common/UIViewControllerExtension.swift
@@ -0,0 +1,18 @@
+//
+//  UIViewControllerExtension.swift
+//  ClipPic
+//
+//  Created by RayShin Lee on 2022/7/13.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func showError(message: String) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+}
